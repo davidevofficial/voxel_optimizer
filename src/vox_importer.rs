62716ffcd7887032202b1f8cded60089ffe24e85
davidevofficial <davidevufficial@gmail.com>
2023-08-17 19:46:57 +0200

    fix

--- src/vox_importer.rs
+++ src/vox_importer.rs
@@ -1,12 +1,39 @@
+use std::error;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
 //Ply reader without using external libraries
-
-//Reads the
+struct v{
+    x: i32,
+    y: i32,
+    z: i32,
+    r: u8,
+    g: u8,
+    b: u8
+}
+struct f{
+    vs: Vec<v>,
+}
+struct ply{
+    //metadata
+    ply_format: String,
+    exported_by: String,
+    number_of_v_and_f: (i32, i32),
+    //vertices and faces
+    vertices: Vec<v>,
+    faces: Vec<f>
+}
+//Reads the ply files and returns the content as a string
 //
 //
 fn read_ply(filepath: String) -> Result<String, io::Error>{
     let mut output = String::new();
-    File::open(filepath)
+    File::open(filepath)?.read_to_string(&mut output)?;
+    Ok(output)
+}
+//Parses the ply file and returns a list of vertices and faces as a list
+//
+//
+fn parse_ply(content: &String) -> Option<ply>{
+    todo!();
 }
--- target/debug/deps/greedy_meshing.d
+++ target/debug/deps/greedy_meshing.d
@@ -1,5 +1,9 @@
-D:\Davide\Archive\scripts di prova\project036 rust\project011-greedy-meshing\greedy-meshing\target\debug\deps\greedy_meshing.exe: src\main.rs
+D:\Davide\Archive\scripts di prova\project036 rust\project011-greedy-meshing\greedy-meshing\target\debug\deps\greedy_meshing.exe: src\main.rs src\vox_importer.rs src\greedy_mesher.rs src\uv_unwrapping.rs src\texture_mapping.rs
 
-D:\Davide\Archive\scripts di prova\project036 rust\project011-greedy-meshing\greedy-meshing\target\debug\deps\greedy_meshing.d: src\main.rs
+D:\Davide\Archive\scripts di prova\project036 rust\project011-greedy-meshing\greedy-meshing\target\debug\deps\greedy_meshing.d: src\main.rs src\vox_importer.rs src\greedy_mesher.rs src\uv_unwrapping.rs src\texture_mapping.rs
 
 src\main.rs:
+src\vox_importer.rs:
+src\greedy_mesher.rs:
+src\uv_unwrapping.rs:
+src\texture_mapping.rs:
